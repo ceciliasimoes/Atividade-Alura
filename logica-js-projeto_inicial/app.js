53332dd010ed1fbe3ffe46cea576454e153e9b6c
aluno <aluno@embarquedigital.portodigital.local>
2024-01-18 16:47:53 -0300

    adicionei loop e contador de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,24 @@
 alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 29;
 console.log(numeroSecreto);
-var numeroEscolhido = window.prompt("Escolha um número entre um e trinta:");
+var numeroEscolhido; 
+let contadorTentativas = 0;
 
-//se o user colocar o múmero certo, ele exibe
-if(numeroSecreto == numeroEscolhido ){
-    alert(`Você descobriu o Número Secreto ${numeroSecreto}`);
-} else{
-    alert("Infelizmente você errou.")
+
+while(numeroEscolhido != numeroSecreto){
+    numeroEscolhido = window.prompt("Escolha um número entre um e trinta:");
+    //se o user colocar o múmero certo, ele exibe
+    if(numeroSecreto == numeroEscolhido ){
+        break;
+    } else{
+        
+        if(numeroEscolhido > numeroSecreto){
+            alert(`O número secreto é menor que ${numeroEscolhido}`);
+        }else{
+            alert(`O número secreto é maior que ${numeroEscolhido}`);
+        }
+        contadorTentativas++;
+    }
 }
+
+alert(`Você descobriu o Número Secreto ${numeroSecreto} em ${contadorTentativas} tentativas!`);
